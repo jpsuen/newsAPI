a19eb939d461a65148acf7f5e776d51f9907de52
Joshua Suen <suen.joshua@gmail.com>
2017-01-13 11:43:47 -0500

    Added a function to clean up dates

--- news_api_helpers.py
+++ news_api_helpers.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def id_fetch(cur):
     x = cur.fetchall()
     x = x[0][0]
@@ -7,3 +10,25 @@ def id_fetch(cur):
 def sanitize(x):
     x = x.replace("'", r"\'").encode('latin-1', 'ignore')
     return x
+
+
+def date_clean(x='None'):
+    str = '%Y-%m-%d %H:%M:%S'
+    default = '2001-01-01 01:01:01'
+    print(x)
+    if x is 'None' or x is 'NULL' or x is None or (int(x[:4]) < 2000):
+        x = datetime.strptime(default, str)
+    else:
+        if len(x) > 18:
+            date = x[:10] + " " + x[11:19]
+            try:
+                x = datetime.strptime(date, str)
+            except ValueError:
+                x = datetime.strptime(default, str)
+        else:
+            x = datetime.strptime(default, str)
+    print(x)
+    return x
+
+
+
