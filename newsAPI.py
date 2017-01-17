b5b25c12dd7ab2c86caf39fa92e1580701ce0203
Joshua Suen <suen.joshua@gmail.com>
2017-01-17 11:45:43 -0500

    Modified code to support Python 2.7.  This was added specifically to enable support for AWS Lambda.

--- newsAPI.py
+++ newsAPI.py
@@ -5,98 +5,102 @@ import requests
 import MySQLdb
 
 
-# Static Vars
-sourceUrl = read_url_config(section='source')
-articlesUrl = read_url_config(section='article')
-sourcePayload = read_news_config(section='source')
-articlePayload = read_news_config(section='article')
+def lambda_function():
+    # Static Vars
+    sourceUrl = read_url_config(section='source')
+    articlesUrl = read_url_config(section='article')
+    sourcePayload = read_news_config(section='source')
+    articlePayload = read_news_config(section='article')
+    read_news_config()
 
-# Check List of Sources
-data = requests.get(sourceUrl, params=sourcePayload).json()
-srcLen = len(data['sources'])
-srcList = data['sources']
+    # Check List of Sources
+    data = requests.get(sourceUrl, params=sourcePayload).json()
+    srcLen = len(data['sources'])
+    srcList = data['sources']
 
-# Open DB Connection
-dbParams = read_db_config()
-db = MySQLdb.connect(**dbParams)
-cur = db.cursor()
+    # Open DB Connection
+    dbParams = read_db_config()
+    db = MySQLdb.connect(**dbParams)
+    cur = db.cursor()
 
-# Get some basic data
-cur.execute("SELECT * FROM sources")
-dbSrc = cur.fetchall()
+    # Get some basic data
+    cur.execute("SELECT * FROM sources")
+    dbSrc = cur.fetchall()
 
-# Check DB for current sources & add, if needed
-if srcLen > len(dbSrc):
+    # Check DB for current sources & add, if needed
+    if srcLen > len(dbSrc):
+        for src in range(0, srcLen):
+            if srcList[src]['id'] not in dbSrc:
+                sourceVars = {'source': sanitize(data['sources'][src]['id']),
+                              "description": sanitize(data['sources'][src]['description'])}
+                sqlInsert = """INSERT INTO sources (source_name, source_description) VALUES
+                ('%(source)s', '%(description)s');""" % sourceVars
+                cur.execute(sqlInsert)
+        db.commit()
+
     for src in range(0, srcLen):
-        if srcList[src]['id'] not in dbSrc:
-            sourceVars = {'source': sanitize(data['sources'][src]['id']),
-                          "description": sanitize(data['sources'][src]['description'])}
-            sqlInsert = """INSERT INTO sources (source_name, source_description) VALUES
-            ('%(source)s', '%(description)s');""" % sourceVars
-            cur.execute(sqlInsert)
-    db.commit()
+        articlePayload['source'] = dbSrc[src][1]
+        data2 = requests.get(articlesUrl, params=articlePayload).json()
+        for art in range(0, len(data2['articles'])):
+            sqlq = "SELECT COUNT(1) FROM articles WHERE url = '%(url)s';" % {'url': data2['articles'][art]['url']}
+            cur.execute(sqlq)
+            if cur.fetchone()[0]:
+                break
+            else:
+                author = data2['articles'][art]['author']
+                if author is None:
+                    authID = 1
+                elif author is not None:
+                    author = sanitize(author)
+                    sqlq = "SELECT COUNT(1) FROM author_list WHERE author_name = '%(name)s';" % {'name': author}
+                    cur.execute(sqlq)
+                    if cur.fetchone()[0]:
+                        cur.execute("SELECT * FROM author_list WHERE author_name = '%(auth)s' LIMIT 1;" % {'auth': author})
+                        authID = id_fetch(cur)
+                    else:
+                        sqlInsert = "INSERT INTO author_list (author_name) VALUES ('%(auth)s');" % {'auth': author}
+                        cur.execute(sqlInsert)
+                        db.commit()
+                        # print(author)
+                        cur.execute("SELECT LAST_INSERT_ID()")
+                        authID = id_fetch(cur)
 
-for src in range(0, srcLen):
-    articlePayload['source'] = dbSrc[src][1]
-    data2 = requests.get(articlesUrl, params=articlePayload).json()
-    for art in range(0, len(data2['articles'])):
-        sqlq = "SELECT COUNT(1) FROM articles WHERE url = '%(url)s';" % {'url': data2['articles'][art]['url']}
-        cur.execute(sqlq)
-        if cur.fetchone()[0]:
-            break
-        else:
-            author = data2['articles'][art]['author']
-            if author is None:
-                authID = 1
-            elif author is not None:
-                author = sanitize(author)
-                sqlq = "SELECT COUNT(1) FROM author_list WHERE author_name = '%(name)s';" % {'name': author}
-                cur.execute(sqlq)
-                if cur.fetchone()[0]:
-                    cur.execute("SELECT * FROM author_list WHERE author_name = '%(auth)s' LIMIT 1;" % {'auth': author})
-                    authID = id_fetch(cur)
-                else:
-                    sqlInsert = "INSERT INTO author_list (author_name) VALUES ('%(auth)s');" % {'auth': author}
-                    cur.execute(sqlInsert)
-                    db.commit()
-                    # print(author)
-                    cur.execute("SELECT LAST_INSERT_ID()")
-                    authID = id_fetch(cur)
+                # Assign variables
+                source_id = dbSrc[src][0]
+                publishedAt = data2['articles'][art]['publishedAt']
+                title = data2['articles'][art]['title']
+                description = data2['articles'][art]['description']
+                url = data2['articles'][art]['url']
+                urlToImage = data2['articles'][art]['urlToImage']
+                articleVars = {'src': source_id, 'pub': publishedAt, 'title': title,
+                               'desc': description, 'url': url, 'urlImg': urlToImage}
+                for v in articleVars.keys():
+                    if articleVars[v] is None:
+                        articleVars[v] = 'NULL'
+                    if type(articleVars[v]) != long:
+                        articleVars[v] = sanitize(articleVars[v])
+                    if v is 'pub':
+                        articleVars[v] = date_clean(articleVars[v])
 
-            # Assign variables
-            source_id = dbSrc[src][0]
-            publishedAt = data2['articles'][art]['publishedAt']
-            title = data2['articles'][art]['title']
-            description = data2['articles'][art]['description']
-            url = data2['articles'][art]['url']
-            urlToImage = data2['articles'][art]['urlToImage']
-            articleVars = {'src': source_id, 'pub': publishedAt, 'title': title,
-                           'desc': description, 'url': url, 'urlImg': urlToImage}
-            for v in articleVars.keys():
-                if articleVars[v] is None:
-                    articleVars[v] = 'NULL'
-                if type(articleVars[v]) != long:
-                    articleVars[v] = sanitize(articleVars[v])
-                if v is 'pub':
-                    articleVars[v] = date_clean(articleVars[v])
+                # Put each article into article table of DB
 
-            # Put each article into article table of DB
+                sqlInsert = """INSERT INTO articles (source_id, publishedAt, title, description, url, urlToImage) VALUES
+                            ('%(src)s', '%(pub)s', '%(title)s', '%(desc)s', '%(url)s', '%(urlImg)s');""" % articleVars
+                cur.execute(sqlInsert)
+                db.commit()
 
-            sqlInsert = """INSERT INTO articles (source_id, publishedAt, title, description, url, urlToImage) VALUES
-                        ('%(src)s', '%(pub)s', '%(title)s', '%(desc)s', '%(url)s', '%(urlImg)s');""" % articleVars
-            cur.execute(sqlInsert)
-            db.commit()
+                # Get article_id from new article
+                cur.execute("SELECT LAST_INSERT_ID()")
+                artID = id_fetch(cur)
 
-            # Get article_id from new article
-            cur.execute("SELECT LAST_INSERT_ID()")
-            artID = id_fetch(cur)
+                # Connect authors with articles in author table
+                artListVars = {'artId': artID, 'autId': authID}
+                sqlInsert = """INSERT INTO author (article_id, author_id) VALUES
+                            ('%(artId)s', '%(autId)s');""" % artListVars
+                cur.execute(sqlInsert)
+                db.commit()
 
-            # Connect authors with articles in author table
-            artListVars = {'artId': artID, 'autId': authID}
-            sqlInsert = """INSERT INTO author (article_id, author_id) VALUES
-                        ('%(artId)s', '%(autId)s');""" % artListVars
-            cur.execute(sqlInsert)
-            db.commit()
+    db.close()
 
-db.close()
+lambda_function()
 
--- python_parse_config.py
+++ python_parse_config.py
@@ -1,70 +1,31 @@
-# Credit to: http://www.mysqltutorial.org/python-connecting-mysql-databases/ for inspiration
-
-from configparser import ConfigParser
+import os
 
 
-def read_db_config(filename='config.ini', section='mysql'):
-    # create parser and read ini configuration file
-    parser = ConfigParser()
-    parser.read(filename)
-
-    # get section, default to mysql
-    db = {}
-    if parser.has_section(section):
-        items = parser.items(section)
-        for item in items:
-            db[item[0]] = item[1]
-    else:
-        raise Exception('{0} not found in the {1} file'.format(section, filename))
+def read_db_config(database='server'):
+    hostname = 'jpsrdsnewsapi.curoecstjrny.us-east-1.rds.amazonaws.com'
+    dbuser = 'newsapi'
+    dbname = 'newsAPI'
+    dbpw = os.environ['newsAPIpw']
+    if database != 'server':
+        dbuser = 'redcap'
+        hostname = 'localhost'
+    db = {'host': hostname, 'user': dbuser, "db": dbname, 'passwd': dbpw}
     return db
 
 
-def read_url_config(filename='config.ini', section='article'):
-    # create parser and read ini configuration file
-    parser = ConfigParser()
-    parser.read(filename)
-
-    # get section, default to article
-    if parser.has_section(section):
-        url = parser.items(section)[0][1]
-    else:
-        raise Exception('{0} not found in the {1} file'.format(section, filename))
+def read_url_config(section='article'):
+    url = 'https://newsapi.org/v1/articles'
+    if section != 'article':
+        url = 'https://newsapi.org/v1/sources'
     return url
 
 
-def read_news_config(filename='config.ini', section='None'):
-    # create parser and read ini configuration file
-    parser = ConfigParser()
-    parser.read(filename)
-
-    # get section
-    api = {}
-    payload = None
-
-    # Always need to grab the token, if it exists
-    if parser.has_section('apiToken'):
-        token = parser.items('apiToken')
-        for t in token:
-            api[t[0]] = t[1]
-    else:
-        raise Exception('{0} not found in the {1} file'.format(section, filename))
-
-    # If searching for article, return those variables
-    if section == 'article':
-        if parser.has_section('articleParams'):
-            payload = parser.items('articleParams')
-        else:
-            raise Exception('{0} not found in the {1} file'.format(section, filename))
-
-    # If searching for sources, return those variables
+def read_news_config(section='None'):
+    apiToken = os.environ['newsAPIToken']
+    api = {'apiKey': apiToken}
     if section == 'source':
-        if parser.has_section('sourceParams'):
-            payload = parser.items('sourceParams')
-        else:
-            raise Exception('{0} not found in the {1} file'.format(section, filename))
-
-    if payload is not None:
-        for l in payload:
-            api[l[0]] = l[1]
-
+        api['category'] = ''
+        api['source'] = ''
+    elif section == 'article':
+        api['source'] = ''
     return api
